4565f2f2ee8e76defb703d009b0afd1c209cbe0e
djouher.kahel <djouher.kahel@etu.u-pec.fr>
2018-01-09 17:55:54 +0100

    diagrammes

---
+++ backoffice/src/test/java/fr/digicar/backoffice/controller/ParkingSpotControllerTest.java
@@ -0,0 +1,17 @@
+package fr.digicar.backoffice.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ParkingSpotControllerTest {
+
+    @Test
+    public void addParkingSpotPage() throws Exception {
+    }
+
+    @Test
+    public void addingParkingSpot() throws Exception {
+    }
+
+}
