bd0af85db5b063752b2643a95e01b6fe0686aa82
Alpha-Ibrahima.BARRY <alpha-ibrahima1.barry@etu.u-pec.fr>
2018-01-14 22:57:32 +0100

    add list of car

--- common/src/main/java/fr/digicar/model/Car.java
+++ common/src/main/java/fr/digicar/model/Car.java
@@ -36,12 +36,12 @@ public class Car implements Serializable{
     @Column(name = "nb_doors")
     private Integer nb_doors;
 
-    @Column(name = "category")
-    private String category;
+    @Column(name = "type")
+    private String type;
 
     /* for location: in parking or storage */
-    @Column(name = "state")
-    private String state;
+    @Column(name = "location")
+    private String location;
 
     @Column(name = "kilometers")
     private Integer kilometers;
@@ -53,6 +53,6 @@ public class Car implements Serializable{
     private Integer comfort;
 
     @Column(name = "fuel_type")
-    private Integer fuel_type;
+    private String fuel_type;
 
 }
