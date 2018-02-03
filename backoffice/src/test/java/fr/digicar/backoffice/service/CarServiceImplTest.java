569617cc4bcee52a3e27144e2436b7149bbca2d0
alpha.barry <alpha-ibrahima1.barry@etu.u-pec.fr>
2018-02-03 23:46:15 +0100

    Add test search

--- backoffice/src/test/java/fr/digicar/backoffice/service/CarServiceImplTest.java
+++ backoffice/src/test/java/fr/digicar/backoffice/service/CarServiceImplTest.java
@@ -72,8 +72,12 @@ public class CarServiceImplTest {
 
     }
 
+    /**
+     *  Unit test for getAllCars method
+     *  from carServiceImpl class
+     */
     @Test
-    public void listAllCarsShouldReturnAllCars(){
+    public void getAllCarsShouldReturnAllCars(){
         //
         // GIVEN
         //
@@ -94,4 +98,142 @@ public class CarServiceImplTest {
         //
         Assert.assertEquals(carList, carListTest);
     }
+
+    /**
+     *  Unit test for getCarByRegistration method
+     *  from carServiceImpl class
+     */
+    @Test
+    public void getCarByRegistrationShouldReturnSpecificCar(){
+        //
+        // GIVEN
+        //
+        Car exceptedCar = new Car(2, "PN-341-KL", "audi", "a6", 2, 4, 5, 1, 40000, "20012-01-14", 5, 3);
+
+        Mockito.when(carDAO.getCarByRegistration("PN-341-KL")).thenReturn(exceptedCar);
+
+        //
+        // WHEN
+        //
+        Car carTest = carService.getCarByRegistration("PN-341-KL");
+
+        //
+        // THEN
+        //
+        Assert.assertEquals(exceptedCar, carTest);
+    }
+
+    /**
+     *  Unit test for getCarByRegistration method
+     *  from carServiceImpl class
+     */
+    @Test
+    public void getCarByRegistrationWithUnknowRegistrationNumberShouldReturnNothing(){
+        //
+        // GIVEN
+        //
+        Car fakeExceptedCar = new Car(2, "PN-341-KL", "audi", "a6", 2, 4, 5, 1, 40000, "20012-01-14", 5, 3);
+
+        Mockito.when(carDAO.getCarByRegistration("PN-341-KL")).thenReturn(fakeExceptedCar);
+
+        //
+        // WHEN
+        //
+        Car carTest = carService.getCarByRegistration("BA-346-PC");
+
+        //
+        // THEN
+        //
+        Assert.assertEquals(null, carTest);
+    }
+
+    /**
+     *  Unit test for carByCriteria method
+     *  from carServiceImpl class
+     */
+    @Test
+    public void carByCriteriaShouldReturnSpecificCars(){
+        //
+        // GIVEN
+        //
+        List<Car> exceptedCarList = new ArrayList<>();
+        exceptedCarList.add(new Car(1, "BA-961-VC", "audi", "a1", 1, 2, 3, 1, 50100, "2009-02-25", 4, 2));
+        exceptedCarList.add(new Car(2, "PN-341-KL", "audi", "a1", 2, 4, 5, 1, 40000, "20012-01-14", 5, 3));
+        exceptedCarList.add(new Car(3, "BD-525-MI", "audi", "a1", 2, 4, 5, 1, 50100, "2009-02-25", 4, 2));
+
+
+        Mockito.when(carDAO.CarByCriteria("audi","a1", null, null, null, null, null)).thenReturn(exceptedCarList);
+
+        //
+        // WHEN
+        //
+        List<Car> carListTest = carService.CarByCriteria("audi","a1", null, null, null, null, null);
+
+        //
+        // THEN
+        //
+        Assert.assertEquals(exceptedCarList, carListTest);
+    }
+
+    /**
+     *  Unit test for carByCriteria method
+     *  from carServiceImpl class
+     */
+    @Test
+    public void carByCriteriaShouldReturnNothingWhenCriteriaNotMatch(){
+        //
+        // GIVEN
+        //
+        List<Car> fakeExceptedCarList = new ArrayList<>();
+        fakeExceptedCarList.add(new Car(1, "BA-961-VC", "audi", "a1", 1, 2, 3, 1, 50100, "2009-02-25", 4, 2));
+        fakeExceptedCarList.add(new Car(2, "PN-341-KL", "audi", "a1", 2, 4, 5, 1, 40000, "20012-01-14", 5, 3));
+        fakeExceptedCarList.add(new Car(3, "BD-525-MI", "audi", "a1", 2, 4, 5, 1, 50100, "2009-02-25", 4, 2));
+
+
+        Mockito.when(carDAO.CarByCriteria("audi","a1", null, null, null, null, null)).thenReturn(fakeExceptedCarList);
+
+        //
+        // WHEN
+        //
+        List<Car> carListTest = carService.CarByCriteria("audi","a5", null, null, null, null, null);
+
+        //
+        // THEN
+        //
+        Assert.assertEquals(new ArrayList<>(), carListTest);
+    }
+
+    /**
+     *  Unit test for carByCriteria method
+     *  from carServiceImpl class
+     */
+    @Test
+    public void carByCriteriaShouldReturnOneCarWhenCriteriaMatchOnOneCar(){
+        //
+        // GIVEN
+        //
+        List<Car> exceptedCarList = new ArrayList<>();
+        exceptedCarList.add(new Car(2, "PN-341-KL", "audi", "a8", 2, 4, 5, 1, 40000, "20012-01-14", 5, 3));
+
+        List<Car> fakeExceptedCarList = new ArrayList<>();
+        fakeExceptedCarList.add(new Car(1, "BA-961-VC", "audi", "a1", 1, 2, 3, 1, 50100, "2009-02-25", 4, 2));
+        fakeExceptedCarList.add(new Car(2, "PN-341-KL", "audi", "a8", 2, 4, 5, 1, 40000, "20012-01-14", 5, 3));
+        fakeExceptedCarList.add(new Car(3, "BD-525-MI", "audi", "a1", 2, 4, 5, 1, 150100, "2009-02-25", 4, 2));
+
+        Mockito.when(carDAO.CarByCriteria("audi","a1", null, null, null, null, null)).thenReturn(fakeExceptedCarList);
+        Mockito.when(carDAO.CarByCriteria("audi","a8", null, null, null, "30000", "45000")).thenReturn(exceptedCarList);
+
+
+        //
+        // WHEN
+        //
+        List<Car> carListTest = carService.CarByCriteria("audi","a8", null, null, null, "30000", "45000");
+
+        //
+        // THEN
+        //
+        Assert.assertEquals(exceptedCarList, carListTest);
+    }
+
+
 }
