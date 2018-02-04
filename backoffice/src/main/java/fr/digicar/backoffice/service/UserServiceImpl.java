a92cfbaf116320d02351a5cc24cad7073cace4d5
evan.suau <evan.suau@etu.u-pec.fr>
2018-02-04 15:48:02 +0100

    Add unit tests and fix condition issue in generateSearchString

--- backoffice/src/main/java/fr/digicar/backoffice/service/UserServiceImpl.java
+++ backoffice/src/main/java/fr/digicar/backoffice/service/UserServiceImpl.java
@@ -32,32 +32,32 @@ public class UserServiceImpl implements UserService {
     String generateSearchString(SearchCriteria searchCriteria) {
         String searchString = "from User where ";
         String byNameString = "";
-        String byDepartmentsString = "";
-        String byStatusesString = "";
+        StringBuilder byDepartmentsString = new StringBuilder();
+        StringBuilder byStatusesString = new StringBuilder();
         if (searchCriteria.getName() != null && searchCriteria.getName().length() > 0) {
             byNameString = "firstName like \'%" + searchCriteria.getName() + "%\' or lastName like \'%" + searchCriteria.getName() + "%\'";
         }
         if (searchCriteria.getDepartments() != null && searchCriteria.getDepartments().length > 0) {
             int i = 0;
             do {
-                byDepartmentsString += i > 0 ? " or " : "";
-                byDepartmentsString += "zipCode like \'" + searchCriteria.getDepartments()[i] + "%\'";
+                byDepartmentsString.append(i > 0 ? " or " : "");
+                byDepartmentsString.append("zipCode like \'").append(searchCriteria.getDepartments()[i]).append("%\'");
                 i++;
             } while (i < searchCriteria.getDepartments().length);
         }
         if (searchCriteria.getStatuses() != null && searchCriteria.getStatuses().length > 0) {
             int i = 0;
             do {
-                byStatusesString += i > 0 ? " or " : "";
-                byStatusesString += "status = \'" + searchCriteria.getStatuses()[i] + "\'";
+                byStatusesString.append(i > 0 ? " or " : "");
+                byStatusesString.append("status = \'").append(searchCriteria.getStatuses()[i]).append("\'");
                 i++;
             } while (i < searchCriteria.getStatuses().length);
         }
         searchString += byNameString.length() > 0 ? byNameString : "";
         searchString += byNameString.length() > 0 && (byDepartmentsString.length() > 0 || byStatusesString.length() > 0) ? " and " : "";
-        searchString += byDepartmentsString.length() > 0 ? byDepartmentsString : "";
-        searchString += (byNameString.length() > 0 || byDepartmentsString.length() > 0) && byStatusesString.length() > 0 ?  " and " : "";
-        searchString += byStatusesString.length() > 0 ? byStatusesString : "";
+        searchString += byDepartmentsString.length() > 0 ? byDepartmentsString.toString() : "";
+        searchString += byDepartmentsString.length() > 0 && byStatusesString.length() > 0 ?  " and " : "";
+        searchString += byStatusesString.length() > 0 ? byStatusesString.toString() : "";
         return searchString;
     }
 
