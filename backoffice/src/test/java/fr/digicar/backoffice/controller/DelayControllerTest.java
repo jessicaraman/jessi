4d0ed69285418a84b64953b301fef64c69d0ff20
evan.suau <evan.suau@etu.u-pec.fr>
2018-03-06 09:03:48 +0100

    Add unit test for delay controller

---
+++ backoffice/src/test/java/fr/digicar/backoffice/controller/DelayControllerTest.java
@@ -0,0 +1,46 @@
+package fr.digicar.backoffice.controller;
+
+import fr.digicar.backoffice.service.DelayService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class DelayControllerTest {
+
+    @Mock
+    private DelayService delayService;
+
+    @InjectMocks
+    private DelayController delayController;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/pages/");
+        viewResolver.setSuffix(".jsp");
+        mockMvc = MockMvcBuilders.standaloneSetup(delayController).setViewResolvers(viewResolver).build();
+    }
+
+    @Test
+    public void showDelayNumber() throws Exception {
+        when(delayService.getDelayNumber()).thenReturn(1000);
+
+        mockMvc.perform(get("/delays"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("delay-analysis"))
+                .andExpect(forwardedUrl("/WEB-INF/pages/delay-analysis.jsp"))
+                .andExpect(model().attribute("delayNumber", 1000));
+    }
+}
