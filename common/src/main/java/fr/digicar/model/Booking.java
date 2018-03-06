736159e7aa3d5cd00458469d42b91b283c6e0f0e
evan.suau <evan.suau@etu.u-pec.fr>
2018-03-06 01:49:23 +0100

    Edit Booking, Delay & Session relations

--- common/src/main/java/fr/digicar/model/Booking.java
+++ common/src/main/java/fr/digicar/model/Booking.java
@@ -1,16 +1,27 @@
 package fr.digicar.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import java.util.Date;
 
 @Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Booking {
 
+    @Id
     private int id;
 
+    @ManyToOne
     private User user;
 
+    @ManyToOne
     private Car car;
 
     private Date expectedDepartureTime;
