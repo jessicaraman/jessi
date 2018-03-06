94124994a950af164abbedb834669c2ffbfbbc82
evan.suau <evan.suau@etu.u-pec.fr>
2018-03-06 09:09:39 +0100

    Add unit test for delay service

---
+++ backoffice/src/test/java/fr/digicar/backoffice/service/DelayServiceImplTest.java
@@ -0,0 +1,37 @@
+package fr.digicar.backoffice.service;
+
+import fr.digicar.dao.DelayDAO;
+import fr.digicar.model.Delay;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DelayServiceImplTest {
+
+    @Mock
+    private DelayDAO delayDAO;
+
+    @InjectMocks
+    private DelayServiceImpl delayService;
+
+    @Test
+    public void getDelayNumber() {
+        List<Delay> delays = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            delays.add(new Delay());
+        }
+        when(delayDAO.findAll()).thenReturn(delays);
+
+        assertEquals(1000, delayService.getDelayNumber());
+    }
+}
