9957e534f3d972da9435f431685950d0e1eb316a
evan.suau <evan.suau@etu.u-pec.fr>
2018-05-12 01:03:15 +0200

    Optimize import on delay controller unit tests

--- backoffice/src/test/java/fr/digicar/backoffice/controller/DelayControllerTest.java
+++ backoffice/src/test/java/fr/digicar/backoffice/controller/DelayControllerTest.java
@@ -25,7 +25,6 @@ import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.hasToString;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
