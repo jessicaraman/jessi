b5494b76b44d84ac9fea96e6d8b05698cae7d664
alpha.barry <alpha-ibrahima1.barry@etu.u-pec.fr>
2018-05-11 18:33:30 +0200

    Add forgotten annotations for fixing 'nested exception is org.hibernate.HibernateException: No Session found for current thread'

--- backoffice/src/main/java/fr/digicar/backoffice/service/ReservationPricesServiceImpl.java
+++ backoffice/src/main/java/fr/digicar/backoffice/service/ReservationPricesServiceImpl.java
@@ -4,10 +4,12 @@ import fr.digicar.dao.ReservationPricesDAO;
 import fr.digicar.model.ReservationPrices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class ReservationPricesServiceImpl implements ReservationPricesService{
 
     @Autowired
